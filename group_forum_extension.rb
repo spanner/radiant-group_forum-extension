fe961dd7e4e548409ccfabcc852c011ab590d949
will <will@spanner.org>
2009-09-10 12:21:47 +0100

    extension requirements declared

--- group_forum_extension.rb
+++ group_forum_extension.rb
@@ -2,6 +2,11 @@ class GroupForumExtension < Radiant::Extension
   version "0.2"
   description "Just a bit of access-control glue joining reader_groups to forum"
   url "http://spanner.org/radiant/group_forums"
+
+  extension_config do |config|
+    config.extension 'forum'
+    config.extension 'reader_group'
+  end
   
   def activate
     Group.send :include, GroupExtensions
