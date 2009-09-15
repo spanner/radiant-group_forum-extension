8e1dd6e70499d5e39e7e9a249e33110aa4641ff4
will <will@spanner.org>
2009-09-15 09:28:03 +0100

    working on sql-only forum retrieval

--- lib/forum_extensions.rb
+++ lib/forum_extensions.rb
@@ -4,6 +4,13 @@ module ForumExtensions
       has_and_belongs_to_many :groups
       alias inherited_groups groups
 
+      named_scope :ungrouped, {
+        :select => 'forums.*',
+        :joins => "left join forums_groups as fg on fg.group_id = forums.id",
+        :group => 'forums.id',
+        :having => 'count(fg.group_id) = 0'
+      }
+      
       def visible_by_default_with_groups?
         groups.empty? && visible_by_default_without_groups?           # ungrouped means visible to all
       end
@@ -19,16 +26,22 @@ module ForumExtensions
       alias_method_chain :visible_to?, :groups
       alias_method_chain :visible_by_default?, :groups
 
+      extend ClassMethods
       include InstanceMethods
     }
   end
   
-  module InstanceMethods     
   
+  module ClassMethods     
+    def visible
+      ungrouped
+    end
+  end
+  
+  module InstanceMethods     
     def has_group?(group)
       return self.groups.include?(group)
     end
-    
   end
 
 end
