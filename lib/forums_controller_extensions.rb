c5348832743f4de9bf5c9dabfe7af592e3a17362
will <will@spanner.org>
2009-11-10 17:21:51 +0000

    more visibility checks pushed down into sql

--- lib/forums_controller_extensions.rb
+++ lib/forums_controller_extensions.rb
@@ -4,6 +4,10 @@ module ForumsControllerExtensions
     base.class_eval {
       before_filter :require_visibility_of_forum
 
+      def index
+        @forums = Forum.visible_to(current_reader).paginate(:all, :order => "position", :page => params[:page] || 1, :per_page => params[:per_page])    #nb. visible_to is a named_scope defined in GroupedModel (in reader_group)
+      end
+
     protected
       
       def require_visibility_of_forum
